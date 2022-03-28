9b26cec5a65770a28c752ec61cfeb39f0be6f0f3
Flavio Rodolfi <flek8rodolfi@gmail.com>
2022-03-28 17:23:06 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $saluto = [
+                "stringa"=> 'Hello World!'
+    ];
+    return view('home', $saluto);
 });
